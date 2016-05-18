d7007fc41f23d076e8d1236cca1fb549b3ccaa20
Alvintorious26 <alvintorious26@gmail.com>
2016-05-18 11:46:00 -0600

    Unity Changes
    Finish at home

---
+++ NEWUNITYPROJECT/DGM 1600 Take Two/Assets/vars.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class vars : MonoBehaviour {
+
+	int jelly = 1;
+	int honey = 5;
+	int peanutbutter = 3;
+	
+	float jellyjar = 0.5f;
+	float honeyjar = 1.7f;
+	float peanutbutterjar = 3.98f;
+	
+	string jellyflavor = "grape";
+	string honeyflavor = "clover";
+	string peanutbutterstyle = "crunchy";
+	
+	int MixIngredients (ingredient)  {
+		return Ingredient1;
+	}
+
+	int MixIngredients (int Ingredient1
+
+	void start () {
+		print (MixIngredients ());
+		print (WhatFoodHasTheMost ());
+		print (GiveInfoOfFood ());
+	}
+}
