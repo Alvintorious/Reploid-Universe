c8a9ac60d2bd0381e2ad13abcdf3dbee73af94c1
Alvintorious <alvintorious26@gmail.com>
2016-05-20 23:13:17 -0600

    Functions and variables Unity
    Did what could be done for now

---
+++ Dgm1600/Assets/DGM1600/Assets/Vars.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;     /* This is the outer most level of the program framework. and is use to make namespace available for the program or app to use
+this namespace is using the system for collections along with unity */
+
+public class Vars : MonoBehaviour 
+{ // This is used in grouping or organizing differant functions and variables together for methods to become a block of data or info
+
+	// Use this for initialization
+	void Start () 
+	{
+
+		int x = 2016;
+
+		string message = "Understood";
+		string upperMessage = message.ToUpper ();
+		Console.WriteLine (upperMessage);
+	
+		float pop = 3.5f;
+	
+	}
+}
