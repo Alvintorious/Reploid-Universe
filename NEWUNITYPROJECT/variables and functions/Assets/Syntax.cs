1f5939bbc13454046eb91997d59b68cb301c1ef5
Alvintorious <alvintorious26@gmail.com>
2016-05-23 11:15:22 -0600

    Syntax example
    What we did in class for syntax lecture

---
+++ NEWUNITYPROJECT/variables and functions/Assets/Syntax.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Syntax : MonoBehaviour 
+{
+
+	int doBeDo = 45;
+
+	int DoBeDo (int _d, int _p) {
+		return _d + _p;
+	}
+
+	int Blablaby2(int _d, int _p) {
+		int _g = (_d + 10) * 2;
+		return _g;
+	}
+	void Start () {
+		print (doBeDo);
+		doBeDo = DoBeDo (45, 27);
+		print (doBeDo);
+		doBeDo = 12;
+		print (doBeDo);
+
+
+
+	}
+
+}
