49605de0055d196c4e77746195388d99eaf2c459
Alvintorious <alvintorious26@gmail.com>
2016-06-20 11:18:26 -0600

    scope

---
+++ NEWUNITYPROJECT/variables and functions/Assets/scope2.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class scope2 : MonoBehaviour {
+
+
+	private scope2  MyFruit;
+
+	void Start () {
+		MyFruit = new scope2 ();
+		MyFruit.AddApples (10);
+		MyFruit.AddApples (10);
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
