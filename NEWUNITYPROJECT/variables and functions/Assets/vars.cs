dcebad9fb19960f66c765bde73e019a7c46737d1
Alvintorious <alvintorious26@gmail.com>
2016-07-06 10:24:29 -0600

    merge

---
+++ NEWUNITYPROJECT/variables and functions/Assets/vars.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class vars : MonoBehaviour {
+
+    int git = 45;
+
+    void Start()
+    {
+        git = ByTwo(git);
+        Debug.Log(git);
+    }
+
+    int ByTwo(int number)
+    {
+        int got;
+
+        got = number * 2;
+
+        return got;
+
+
+    }
+}
